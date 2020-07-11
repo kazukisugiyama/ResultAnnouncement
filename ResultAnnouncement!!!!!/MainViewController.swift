ccda8841758ba96c59c06db70dc05d288a76d47e
杉山和輝 <s.kgmgmjtd579@gmail.com>
2020-07-12 01:31:09 +0900

    UIモック作成、各ViewController作成

---
+++ ResultAnnouncement!!!!!/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResultAnnouncement!!!!!
+//
+//  Created by 杉山和輝 on 2020/07/11.
+//  Copyright © 2020 杉山和輝. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ResultAnnouncement!!!!!/RuleSettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RuleSettingViewController.swift
+//  ResultAnnouncement!!!!!
+//
+//  Created by 杉山和輝 on 2020/07/12.
+//  Copyright © 2020 杉山和輝. All rights reserved.
+//
+
+import UIKit
+
+class RuleSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
