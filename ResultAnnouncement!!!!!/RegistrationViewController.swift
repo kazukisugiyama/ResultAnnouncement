f89ede419ccebbf7c1c88121ccb861bf09e34fa2
杉山和輝 <s.kgmgmjtd579@gmail.com>
2020-07-14 19:54:43 +0900

    メールアドレス、パスワード情報登録画面の実装

--- ResultAnnouncement!!!!!/RegistrationViewController.swift
+++ ResultAnnouncement!!!!!/RegistrationViewController.swift
@@ -7,24 +7,78 @@
 //
 
 import UIKit
+import Firebase
 
 class RegistrationViewController: UIViewController {
 
+    @IBOutlet weak var alertMailAddresLabel: UILabel!
+    @IBOutlet weak var alertPasswordLabel: UILabel!
+    @IBOutlet weak var alertPasswordReEnterLabel: UILabel!
+    @IBOutlet weak var mailAddresTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordReEnterTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        alertMailAddresLabel.isHidden = true
+        alertPasswordLabel.isHidden = true
+        alertPasswordReEnterLabel.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerButton(_ sender: Any) {
+        // 入力チェック
+        noInputCheck()
+        passwordInputCheck()
+        
+        guard let mailAddresText = mailAddresTextField.text,
+            let passwordText = passwordTextField.text else { return }
+        
+        // Firebaseにメールアドレス、パスワードを登録
+        Auth.auth().createUser(withEmail: mailAddresText, password: passwordText) { (authDataResult, error) in
+            if let error = error {
+                print("error : \(error)")
+            } else {
+                print("user create complete")
+                // チャット画面に遷移
+                self.performSegue(withIdentifier: "ruleSetting", sender: nil)
+            }
+        }
     }
-    */
-
+    
+    func noInputCheck() {
+        var isInput = true
+        
+        if mailAddresTextField.text == "" {
+            alertMailAddresLabel.isHidden = false
+            isInput = false
+        } else {
+            alertMailAddresLabel.isHidden = true
+        }
+        if passwordTextField.text == "" {
+            alertPasswordLabel.isHidden = false
+            isInput = false
+        } else {
+            alertPasswordLabel.isHidden = true
+        }
+        if passwordReEnterTextField.text == "" {
+            alertPasswordReEnterLabel.isHidden = false
+            isInput = false
+        } else {
+            alertPasswordReEnterLabel.isHidden = true
+        }
+        
+        guard isInput else { return }
+    }
+    
+    func passwordInputCheck() {
+        guard passwordTextField.text == passwordReEnterTextField.text else {
+            alertPasswordLabel.isHidden = false
+            alertPasswordLabel.text = "パスワード再入力と一致しません"
+            alertPasswordReEnterLabel.isHidden = false
+            alertPasswordReEnterLabel.text = "パスワードと一致しません"
+            return
+        }
+    }
+    
 }
